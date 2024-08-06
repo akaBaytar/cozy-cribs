9565123593cfa8cbb3b14b0ef0c4153840ef00b0
Burak Bilgili <burak.bilgili@icloud.com>
2024-08-06 18:37:58 +0300

    change favicon and add logo to navbar

--- components/navbar/Logo.tsx
+++ components/navbar/Logo.tsx
@@ -1,7 +1,17 @@
+import Link from 'next/link';
+import Image from 'next/image';
+
+import { Button } from '../ui/button';
+import CozyCribs from '@/public/logo.png';
+
 const Logo = () => {
   return (
-    <div>Logo</div>
-  )
-}
+    <Button size='icon' variant='link' asChild>
+      <Link href='/'>
+        <Image src={CozyCribs} width={40} alt='Home Page - CozyCribs' />
+      </Link>
+    </Button>
+  );
+};
 
-export default Logo
+export default Logo;
