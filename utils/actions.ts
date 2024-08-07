158de97a7c2db8fb5cf759eeb79baa31e0f344e8
Burak Bilgili <burak.bilgili@icloud.com>
2024-08-07 19:23:45 +0300

    create new user functionality

--- utils/actions.ts
+++ utils/actions.ts
@@ -1,14 +1,38 @@
 'use server';
 
+import { redirect } from 'next/navigation';
+import { revalidatePath } from 'next/cache';
+
 import { ZodError } from 'zod';
+import { auth, clerkClient, currentUser } from '@clerk/nextjs/server';
+
+import prisma from './database';
 import { profileSchema } from './schemas';
 
 export const createProfile = async (prevState: any, formData: FormData) => {
   try {
+    const user = await currentUser();
+
+    if (!user) throw new Error('Please login.');
+
     const rawData = Object.fromEntries(formData);
     const validatedFields = profileSchema.parse(rawData);
 
-    return { message: 'Profile created successfully.' };
+    await prisma.profile.create({
+      data: {
+        clerkId: user.id,
+        email: user.emailAddresses[0].emailAddress || validatedFields.email,
+        profileImage: user.imageUrl ?? '',
+        firstName: validatedFields.firstName,
+        lastName: validatedFields.lastName,
+      },
+    });
+
+    await clerkClient.users.updateUserMetadata(user.id, {
+      privateMetadata: {
+        hasProfile: true,
+      },
+    });
   } catch (error) {
     let message = 'An error occurred.';
 
@@ -16,4 +40,6 @@ export const createProfile = async (prevState: any, formData: FormData) => {
 
     return { message };
   }
+
+  redirect('/');
 };
