ffbcb330aab10052683f35a176f59d3f2de1c98e
Burak Bilgili <burak.bilgili@icloud.com>
2024-08-08 20:02:18 +0300

    setup property validation via zod

--- utils/schemas.ts
+++ utils/schemas.ts
@@ -31,3 +31,44 @@ export const profileSchema = z.object({
 export const imageSchema = z.object({
   image: validateFile(),
 });
+
+export const propertySchema = z.object({
+  name: z
+    .string()
+    .min(3, { message: 'Property name must be at least 3 characters.' })
+    .max(40, { message: 'Property name can be a maximum of 40 characters.' }),
+  tagline: z
+    .string()
+    .min(3, { message: 'Property name must be at least 3 characters.' })
+    .max(40, { message: 'Property name can be a maximum of 40 characters.' }),
+  price: z.coerce
+    .number()
+    .int()
+    .min(0, { message: 'Price must be a positive number.' }),
+  category: z.string(),
+  description: z.string().refine(
+    (desc) => {
+      const count = desc.split(' ').length;
+      return count >= 10 && count <= 500;
+    },
+    { message: 'Property description must be between 10 and 500 words.' }
+  ),
+  country: z.string(),
+  guests: z.coerce
+    .number()
+    .int()
+    .min(0, { message: 'Guest amount must be a positive number.' }),
+  bedrooms: z.coerce
+    .number()
+    .int()
+    .min(0, { message: 'Bedrooms amount must be a positive number.' }),
+  beds: z.coerce
+    .number()
+    .int()
+    .min(0, { message: 'Beds amount must be a positive number.' }),
+  baths: z.coerce
+    .number()
+    .int()
+    .min(0, { message: 'Baths amount must be a positive number.' }),
+  amenities: z.string(),
+});
