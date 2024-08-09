837e25953cfe3fabcb56e7c4e19afe969949683c
Burak Bilgili <burak.bilgili@icloud.com>
2024-08-09 17:14:09 +0300

    setup property card types and fetch properties actions

---
+++ actions/fetchProperties.ts
@@ -0,0 +1,34 @@
+'use server';
+
+import prisma from '@/utils/database';
+
+type propTypes = {
+  search?: string;
+  category?: string;
+};
+
+export const fetchProperties = async ({ search = '', category }: propTypes) => {
+  const properties = await prisma.property.findMany({
+    where: {
+      category: category,
+      OR: [
+        { name: { contains: search, mode: 'insensitive' } },
+        { tagline: { contains: search, mode: 'insensitive' } },
+        { country: { contains: search, mode: 'insensitive' } },
+      ],
+    },
+    select: {
+      id: true,
+      name: true,
+      tagline: true,
+      country: true,
+      price: true,
+      image: true,
+    },
+    orderBy: {
+      createdAt: 'desc',
+    },
+  });
+
+  return properties;
+};
