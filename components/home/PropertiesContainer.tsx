97cb629f55aa5414b3225114f44460f9f652c11b
Burak Bilgili <burak.bilgili@icloud.com>
2024-08-10 16:47:38 +0300

    properties container component

--- components/home/PropertiesContainer.tsx
+++ components/home/PropertiesContainer.tsx
@@ -1,10 +1,31 @@
+import PropertiesList from './PropertiesList';
+import EmptyList from './EmptyList';
+
+import { fetchProperties } from '@/actions/fetchProperties';
+import type { PropertyCard } from '@/utils/types';
+
 type ContainerProps = {
   category?: string;
   search?: string;
 };
 
-const PropertiesContainer = ({ category, search }: ContainerProps) => {
-  return <div>PropertiesContainer</div>;
+const PropertiesContainer = async ({ category, search }: ContainerProps) => {
+  const properties: PropertyCard[] = await fetchProperties({
+    category,
+    search,
+  });
+
+  if (properties.length === 0) {
+    return (
+      <EmptyList
+        heading='No results found'
+        message='Try changing or removing the filters.'
+        buttonText='Clear Filters'
+      />
+    );
+  }
+
+  return <PropertiesList properties={properties} />;
 };
 
 export default PropertiesContainer;
