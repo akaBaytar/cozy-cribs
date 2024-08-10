4f692e7e4530c1d1a5be698544c7bebd84931a32
Burak Bilgili <burak.bilgili@icloud.com>
2024-08-10 16:59:41 +0300

    visual bug fixes

--- components/home/CategoriesList.tsx
+++ components/home/CategoriesList.tsx
@@ -14,13 +14,13 @@ const CategoriesList = ({ category, search }: ListProps) => {
   return (
     <section>
       <ScrollArea className='py-6'>
-        <div className='flex gap-x-4'>
+        <div className='flex gap-x-16 justify-between'>
           {categories.map((each) => (
             <Link
               href={`/?category=${each.label}${searchTerm}`}
               key={each.label}>
               <article
-                className={`flex flex-col items-center p-3 w-24 cursor-pointer duration-200 hover:text-primary ${
+                className={`flex flex-col items-center py-4 cursor-pointer duration-200 hover:text-primary ${
                   each.label === category ? 'text-primary' : ''
                 }`}>
                 <each.icon className='w-8 h-8' />
@@ -29,7 +29,7 @@ const CategoriesList = ({ category, search }: ListProps) => {
             </Link>
           ))}
         </div>
-        <ScrollBar orientation='horizontal'/>
+        <ScrollBar orientation='horizontal' />
       </ScrollArea>
     </section>
   );
