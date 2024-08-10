de56acb1beb3c517cb352e21f6829b293a4538fe
Burak Bilgili <burak.bilgili@icloud.com>
2024-08-10 16:32:52 +0300

    empty list component

--- components/home/EmptyList.tsx
+++ components/home/EmptyList.tsx
@@ -1,7 +1,23 @@
-const EmptyList = () => {
-  return (
-    <div>EmptyList</div>
-  )
-}
+import Link from 'next/link';
 
-export default EmptyList
+import { Button } from '../ui/button';
+
+type PropTypes = {
+  heading?: string;
+  message?: string;
+  buttonText?: string;
+};
+
+const EmptyList = ({ heading, message, buttonText }: PropTypes) => {
+  return (
+    <div className='mt-4'>
+      <h2 className='text-xl font-bold'>{heading}</h2>
+      <p className='text-lg mt-4'>{message}</p>
+      <Button asChild className='mt-4' size='lg'>
+        {<Link href='/'>{buttonText}</Link>}
+      </Button>
+    </div>
+  );
+};
+
+export default EmptyList;
