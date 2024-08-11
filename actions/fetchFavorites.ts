c59d380f1940661ead0382b6af62b990db8b8d5b
Burak Bilgili <burak.bilgili@icloud.com>
2024-08-11 20:53:42 +0300

    fetch favorite properties action

---
+++ actions/fetchFavorites.ts
@@ -0,0 +1,26 @@
+'use server';
+
+import prisma from '@/utils/database';
+import { getAuthUser } from '@/helpers/getAuthUser';
+
+export const fetchFavorites = async () => {
+  const user = await getAuthUser();
+
+  const favorites = await prisma.favorite.findMany({
+    where: { profileId: user?.id },
+    select: {
+      property: {
+        select: {
+          id: true,
+          name: true,
+          tagline: true,
+          country: true,
+          price: true,
+          image: true,
+        },
+      },
+    },
+  });
+
+  return favorites.map((favorite) => favorite.property);
+};
