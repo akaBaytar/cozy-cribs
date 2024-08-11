5efc3c51f3d41267bc8fc345469453995cff99eb
Burak Bilgili <burak.bilgili@icloud.com>
2024-08-11 21:08:18 +0300

    favorites page done

---
+++ app/favorites/loading.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+import Loading from '@/components/card/Loading';
+
+const FavoritesLoading = () => {
+  return <Loading />;
+};
+
+export default FavoritesLoading;
--- app/favorites/page.tsx
+++ app/favorites/page.tsx
@@ -1,5 +1,21 @@
-const Favorites = () => {
-  return <div>Favorites</div>;
+import EmptyList from '@/components/home/EmptyList';
+import PropertiesList from '@/components/home/PropertiesList';
+
+import { fetchFavorites } from '@/actions/fetchFavorites';
+
+const Favorites = async () => {
+  const favorites = await fetchFavorites();
+
+  if (favorites.length === 0)
+    return (
+      <EmptyList
+        heading='Your favorites list is empty'
+        message='Browse the properties and favorite the ones you’re interested in.'
+        buttonText='Browse the Properties'
+      />
+    );
+
+  return <PropertiesList properties={favorites} />;
 };
 
 export default Favorites;
