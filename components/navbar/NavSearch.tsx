892cf0283db3076f59c6af82ceab98c9ea447259
Burak Bilgili <burak.bilgili@icloud.com>
2024-08-11 16:50:36 +0300

    search functionality with 500ms delay

--- components/navbar/NavSearch.tsx
+++ components/navbar/NavSearch.tsx
@@ -1,12 +1,41 @@
+'use client';
+
+import { useState, useEffect } from 'react';
+import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+
+import { useDebouncedCallback } from 'use-debounce';
 import { Input } from '../ui/input';
 
 const NavSearch = () => {
+  const params = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
+  const [search, setSearch] = useState(params.get('search')?.toString() || '');
+
+  const propertySearch = useDebouncedCallback((value: string) => {
+    const searchParams = new URLSearchParams(params);
+
+    value ? searchParams.set('search', value) : searchParams.delete('search');
+
+    replace(`${pathname}?${searchParams.toString()}`);
+  }, 500);
+
+  useEffect(() => {
+    if (!params.get('search')) setSearch('');
+  }, [params]);
+
   return (
     <Input
       id='searchBar'
       type='text'
       placeholder='Search...'
       className='max-w-xs dark:bg-muted'
+      value={search}
+      onChange={(e) => {
+        setSearch(e.target.value);
+        propertySearch(e.target.value);
+      }}
     />
   );
 };
