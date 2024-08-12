c1f409d2adb8f93adcae06616e54dfe32d3a8df4
Burak Bilgili <burak.bilgili@icloud.com>
2024-08-12 13:57:58 +0300

    visual improvements

--- app/properties/loading.tsx
+++ app/properties/loading.tsx
@@ -1,9 +1,17 @@
 'use client';
 
+import { Fragment } from 'react';
 import { Skeleton } from '@/components/ui/skeleton';
 
 const Loading = () => {
-  return <Skeleton className='h-[300px] md:h-[500px] w-full rounded' />;
+  return (
+    <Fragment>
+      <Skeleton className='h-[12px] w-full rounded' />
+      <Skeleton className='h-[16px] md:h-[20px] w-full rounded mt-8' />
+      <Skeleton className='h-[16px] md:h-[20px] w-full rounded mt-4' />
+      <Skeleton className='h-[300px] md:h-[500px] w-full rounded mt-8' />
+    </Fragment>
+  );
 };
 
 export default Loading;
