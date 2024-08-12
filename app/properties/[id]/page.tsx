c67e6a24057ccd3e5eed92923c08aef9c673471b
Burak Bilgili <burak.bilgili@icloud.com>
2024-08-12 12:36:03 +0300

    breadcrumbs component

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -1,4 +1,8 @@
 import { redirect } from 'next/navigation';
+
+import Breadcrumbs from '@/components/properties/Breadcrumbs';
+import FavoriteButton from '@/components/card/FavoriteButton';
+
 import { fetchPropertyDetails } from '@/actions/fetchPropertyDetails';
 
 const PropertyDetailsPage = async ({ params }: { params: { id: string } }) => {
@@ -6,10 +10,26 @@ const PropertyDetailsPage = async ({ params }: { params: { id: string } }) => {
 
   if (!property) redirect('/');
 
+  const { category, name, tagline, id } = property;
+
   const { baths, bedrooms, beds, guests } = property;
   const details = { baths, bedrooms, beds, guests };
 
-  return <div>PropertyDetailsPage</div>;
+  return (
+    <section>
+      <Breadcrumbs category={category} name={name} />
+      <header className='flex justify-between items-center mt-4'>
+        <article>
+          <h1 className='text-2xl font-semibold'>{name}</h1>
+          <h2 className='text-muted-foreground'>{tagline}</h2>
+        </article>
+        <div className='flex items-center gap-4'>
+          {/* TODO =====> SHARE BUTTON */}
+          <FavoriteButton propertyId={id} />
+        </div>
+      </header>
+    </section>
+  );
 };
 
 export default PropertyDetailsPage;
---
+++ components/properties/Breadcrumbs.tsx
@@ -0,0 +1,37 @@
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from '../ui/breadcrumb';
+
+type PropTypes = {
+  name: string;
+  category: string;
+};
+
+const Breadcrumbs = ({ name, category }: PropTypes) => {
+  return (
+    <Breadcrumb>
+      <BreadcrumbList>
+        <BreadcrumbItem>
+          <BreadcrumbLink href='/'>Home</BreadcrumbLink>
+        </BreadcrumbItem>
+        <BreadcrumbSeparator />
+        <BreadcrumbItem>
+          <BreadcrumbLink
+            href={`/?category=${category}`}
+            className='capitalize'>
+            {category}
+          </BreadcrumbLink>
+        </BreadcrumbItem>
+        <BreadcrumbSeparator />
+        <BreadcrumbPage>{name}</BreadcrumbPage>
+      </BreadcrumbList>
+    </Breadcrumb>
+  );
+};
+
+export default Breadcrumbs;
