abdc65c563eb9105deae7ac30dd6a32f5f5d021e
Burak Bilgili <burak.bilgili@icloud.com>
2024-08-15 11:44:10 +0300

    review action functions completed

---
+++ actions/deleteReview.ts
@@ -0,0 +1,30 @@
+'use server';
+
+import { revalidatePath } from 'next/cache';
+
+import prisma from '@/utils/database';
+import { getAuthUser } from '@/helpers/getAuthUser';
+import { renderError } from '@/helpers/renderError';
+
+export const deleteReview = async (prevState: { reviewId: string }) => {
+  const { reviewId } = prevState;
+
+  const user = await getAuthUser();
+
+  if (!user) throw new Error('You must log in first to delete a review.');
+
+  try {
+    await prisma.review.delete({
+      where: {
+        id: reviewId,
+        profileId: user.id,
+      },
+    });
+
+    revalidatePath('/reviews');
+
+    return { message: 'Review deleted successfully.' };
+  } catch (error) {
+    return renderError(error);
+  }
+};
---
+++ actions/fetchReviewByUser.ts
@@ -0,0 +1,29 @@
+'use server';
+
+import prisma from '@/utils/database';
+import { getAuthUser } from '@/helpers/getAuthUser';
+
+export const fetchPropertyByUser = async () => {
+  const user = await getAuthUser();
+
+  if (!user) throw new Error('You must log in first to see a your reviews.');
+
+  const reviews = await prisma.review.findMany({
+    where: {
+      profileId: user.id,
+    },
+    select: {
+      id: true,
+      rating: true,
+      comment: true,
+      property: {
+        select: {
+          name: true,
+          image: true,
+        },
+      },
+    },
+  });
+
+  return reviews;
+};
