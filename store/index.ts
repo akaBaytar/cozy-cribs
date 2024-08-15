8a4e80d5e1dc3dfef9323cf8ec7b057596f0fb14
Burak Bilgili <burak.bilgili@icloud.com>
2024-08-15 16:06:19 +0300

    create zustand store

---
+++ store/index.ts
@@ -0,0 +1,15 @@
+import { create } from 'zustand';
+import { DateRange } from 'react-day-picker';
+
+import type { Booking } from '@/types';
+
+type PropertyState = {
+  propertyId: string;
+  price: number;
+  bookings: Booking[];
+  range: DateRange | undefined;
+};
+
+export const useProperty = create<PropertyState>(() => {
+  return { propertyId: '', price: 0, bookings: [], range: undefined };
+});
