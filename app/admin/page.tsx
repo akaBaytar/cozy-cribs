7f2db87d1eef5227f84c644d178531faa056648e
Burak Bilgili <burak.bilgili@icloud.com>
2024-08-16 17:31:30 +0300

    admin page structure and  suspense components

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,5 +1,22 @@
+import { Suspense } from 'react';
+
+import StatsContainer from '@/components/admin/StatsContainer';
+import ChartsContainer from '@/components/admin/ChartsContainer';
+
+import { StatsLoadingContainer } from '@/components/admin/Loading';
+import { ChartsLoadingContainer } from '@/components/admin/Loading';
+
 const AdminPage = () => {
-  return <div>AdminPage</div>;
+  return (
+    <>
+      <Suspense fallback={<StatsLoadingContainer />}>
+        <StatsContainer />
+      </Suspense>
+      <Suspense fallback={<ChartsLoadingContainer />}>
+        <ChartsContainer />
+      </Suspense>
+    </>
+  );
 };
 
 export default AdminPage;
--- components/admin/Loading.tsx
+++ components/admin/Loading.tsx
@@ -1,5 +1,32 @@
+import { Card, CardHeader } from '../ui/card';
+import { Skeleton } from '../ui/skeleton';
+
 const Loading = () => {
   return <div>Loading</div>;
+};
+
+const LoadingCard = () => {
+  return (
+    <Card>
+      <CardHeader>
+        <Skeleton className='h-20 w-full rounded-md' />
+      </CardHeader>
+    </Card>
+  );
+};
+
+export const StatsLoadingContainer = () => {
+  return (
+    <div className='grid md:grid-cols-3 mt-8 gap-4'>
+      <LoadingCard />
+      <LoadingCard />
+      <LoadingCard />
+    </div>
+  );
+};
+
+export const ChartsLoadingContainer = () => {
+  return <Skeleton className='mt-8 w-full h-[300px] rounded-md' />;
 };
 
 export default Loading;
