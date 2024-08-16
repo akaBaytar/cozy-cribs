2bab09a17455267efc2fbe470d0fd43faa9abd12
Burak Bilgili <burak.bilgili@icloud.com>
2024-08-16 18:37:58 +0300

    add monthly bookings chart to admin page

--- components/admin/Chart.tsx
+++ components/admin/Chart.tsx
@@ -1,5 +1,40 @@
-const Chart = () => {
-  return <div>Chart</div>;
+'use client';
+
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  ResponsiveContainer,
+} from 'recharts';
+
+type PropType = {
+  data: {
+    date: string;
+    count: number;
+  }[];
+};
+
+const Chart = ({ data }: PropType) => {
+  return (
+    <section className='mt-8'>
+      <h1 className='text-2xl font-semibold text-center'>Monthly Bookings</h1>
+      <ResponsiveContainer width='100%' height={300}>
+        <BarChart data={data} margin={{ top: 30 }}>
+          <CartesianGrid strokeDasharray='3 3' />
+          <XAxis dataKey='date' />
+          <YAxis allowDecimals={false} />
+          <Bar
+            dataKey='count'
+            fill='#f97215'
+            barSize={50}
+            className='rounded-md'
+          />
+        </BarChart>
+      </ResponsiveContainer>
+    </section>
+  );
 };
 
 export default Chart;
--- components/admin/ChartsContainer.tsx
+++ components/admin/ChartsContainer.tsx
@@ -1,5 +1,12 @@
-const ChartsContainer = () => {
-  return <div>ChartsContainer</div>;
+import Chart from './Chart';
+import { fetchChartsData } from '@/actions/fetchChartsData';
+
+const ChartsContainer = async () => {
+  const bookings = await fetchChartsData();
+
+  if (bookings.length < 1) return null;
+
+  return <Chart data={bookings} />;
 };
 
 export default ChartsContainer;
