5d40ae4dfaa2adcac1975f36672a569ef650627d
Burak Bilgili <burak.bilgili@icloud.com>
2024-08-16 14:01:54 +0300

    update property image action completed

--- actions/updateRentalImage.ts
+++ actions/updateRentalImage.ts
@@ -1,11 +1,43 @@
 'use server';
 
-import { getAuthUser } from '@/helpers/getAuthUser';
+import { revalidatePath } from 'next/cache';
 
-export const updateRentalImage = async () => {
+import prisma from '@/database';
+import { imageSchema } from '@/schemas';
+import { uploadImage } from '@/utils/supabase';
+
+import { getAuthUser } from '@/helpers/getAuthUser';
+import { renderError } from '@/helpers/renderError';
+import { validateFields } from '@/helpers/validateFields';
+
+export const updateRentalImage = async (_: any, formData: FormData) => {
   const user = await getAuthUser();
 
   if (!user) throw new Error('You must log in first to create a rental.');
-  
-  return { message: 'update property image action' };
+
+  const propertyId = formData.get('id') as string;
+
+  try {
+    const image = formData.get('image') as File;
+
+    const validatedFields = validateFields(imageSchema, image);
+
+    const path = await uploadImage(validatedFields.image);
+
+    await prisma.property.update({
+      where: {
+        id: propertyId,
+        profileId: user.id,
+      },
+      data: {
+        image: path,
+      },
+    });
+
+    revalidatePath(`/rentals/${propertyId}/edit`);
+
+    return { message: 'Property image updated successfully.' };
+  } catch (error) {
+    return renderError(error);
+  }
 };
